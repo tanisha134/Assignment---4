aab8bff889129dc885f52f7051b0a20ddab015c6
Tanisha <jannatulnurtanisha63@gmail.com>
2026-02-24 21:20:30 +0600

    Added final section and fixed some mistakes

--- script.js
+++ script.js
@@ -14,28 +14,28 @@ const container =  document.getElementById("jobContainer");
 const emptyMassage = document.getElementById("emptyMassage");
 
 function updateDashboard(){
-    document.getElementById("totalCount").innerText = jobs.length;
+    document.getElementById("totalCount").innerText=jobs.length;
     document.getElementById("appliedCount").innerText = jobs.filter(j=>j.status==="applied").length;
     document.getElementById("interviewCount").innerText = jobs.filter(j=>j.status==="interview").length;
     document.getElementById("rejectedCount").innerText = jobs.filter(j=>j.status==="rejected").length;
 }
 
 function getBadge(status){
-    if(status === "applied") return `<span class = "px-3 py-1 text-xs bg-blue-500 text-white rounded">APPLIED</span>`;
-    if(status === "interview") return `<span class = "px-3 py-1 text-xs bg-green-500 text-white rounded">INTERVIEW</span>`;
-    if(status === "rejected") return `<span class = "px-3 py-1 text-xs bg-red-500 text-white rounded">REJECTED</span>`;
-    return `<span class = "px-3 py-1 text-xs bg-[#EEF4FF] text-white rounded">NOT APPLIED</span>`;
+    if(status ==="applied") return `<span class ="px-3 py-1 text-xs bg-blue-500 text-white rounded">APPLIED</span>`;
+    if(status ==="interview") return `<span class ="px-3 py-1 text-xs bg-green-500 text-white rounded">INTERVIEW</span>`;
+    if(status ==="rejected") return `<span class ="px-3 py-1 text-xs bg-red-500 text-white rounded">REJECTED</span>`;
+    return `<span class ="px-3 py-1 text-xs bg-[#EEF4FF] text-black rounded">NOT APPLIED</span>`;
 }
 
 function renderJobs(){
     container.innerHTML ="";
 
-    const filtered = jobs.filter(job=>{
-        if(currentTab === "all") return true;
-        return job.status === currentTab;
+    const filtered=jobs.filter(job=>{
+        if(currentTab==="all") return true;
+        return job.status===currentTab;
     });
 
-    document.getElementById("sectionCount").innerText = filtered.length;
+    document.getElementById("sectionCount").innerText=filtered.length;
 
     if(filtered.length===0){
         emptyMassage.classList.remove("hidden");
@@ -45,30 +45,66 @@ function renderJobs(){
     }
 
     filtered.forEach(job=>{
-        const card = document.createElement("div");
-        card.className = "bg-white p-6 rounded shadow relative border";
+        const card=document.createElement("div");
+        card.className="bg-white p-6 rounded shadow relative border";
 
         card.innerHTML=`
-            <button class = "delete-btn text-[#64748B] delete-btn absolute top-4 right-4 bg-white border border-[#64748B] rounded-full w-8 h-8 flex items-center justify-center shadow-sm hover:bg-gray-200 transition duration-200"" data-id="${job.id}"><i class="fa-regular fa-trash-can"></i> </button>
-            <div class = "flex justify-between items-center pr-8">
-                <h3 class = "text-lg font-bold">${job.company}</h3>
+            <button class="delete-btn text-[#64748B] delete-btn absolute top-4 right-4 bg-white border border-gray-300 rounded-full w-8 h-8 flex items-center justify-center shadow-sm hover:bg-gray-100 transition duration-200"" data-id="${job.id}"><i class="fa-regular fa-trash-can"></i> </button>
+            <div class="flex justify-between items-center pr-8">
+                <h3 class="text-lg font-bold">${job.company}</h3>
                 ${getBadge(job.status)}
             </div>
-            <p class = "text-sm text-[#323B49]">${job.position}</p>
-            <p class = "text-sm text-[#64748B]">${job.position} • ${job.type} • ${job.salary}</p>
-            <p class = "mt-3 text-sm text-[#64748B]">${job.description}</p>
+            <p class = "text-sm text-gray-600">${job.position}</p>
+
+            <p class = "text-sm text-[#64748B]">${job.location} • ${job.type} • ${job.salary}</p>
+            <p class="mt-3 text-sm text-[#64748B]">${job.description}</p>
 
 
             <div class= "flex gap-2 mt-4">
-                <button class= "status-btn bg-[#F8FAFC] px-3 py-2 text-sm rounded" data-status="applied" data-id="${job.id}">Apply</button>
+                <button class= "status-btn bg-blue-100 px-3 py-1 text-sm rounded" data-status="applied"  data-id="${job.id}">Apply</button>
 
-                <button class= "status-btn bg-[#F8FAFC] px-3 py-2 text-sm rounded" data-status="interview" data-id="${job.id}">Interview</button>
+                <button class= "status-btn bg-green-100 px-3 py-2 text-sm rounded" data-status="interview" data-id="${job.id}">Interview</button>
 
-                <button class= "status-btn bg-[#F8FAFC] px-3 py-2 text-sm rounded" data-status="rejected" data-id="${job.id}">Reject</button>
+                <button class= "status-btn bg-red-100 px-3 py-2 text-sm rounded" data-status="rejected" data-id="${job.id}">Reject</button>
             </div>
         `;
 
         container.appendChild(card);
     });
+
     updateDashboard();
 }
+
+
+container.addEventListener("click", function(e){
+    const id=parseInt(e.target.dataset.id);
+    const job=jobs.find(j=>j.id===id);
+
+    if(e.target.classList.contains("status-btn")){
+        job.status=e.target.dataset.status;
+    }
+
+    if(e.target.classList.contains("delete-btn")){
+        const index=jobs.findIndex(j=>j.id===id);
+        jobs.splice(index,1);
+    }
+
+    renderJobs();
+});
+
+document.querySelectorAll(".tab-btn").forEach(btn=>{
+    btn.addEventListener("click", function(){
+        document.querySelectorAll(".tab-btn").forEach(b=>{
+            b.classList.remove("bg-black", "text-white");
+            b.classList.add("bg-gray-200");
+        });
+
+        this.classList.add("bg-black", "text-white");
+        this.classList.remove("bg-black-200");
+
+        currentTab=this.dataset.tab;
+        renderJobs();
+    });
+});
+
+renderJobs()
